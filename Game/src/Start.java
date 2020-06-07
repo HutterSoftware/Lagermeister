d90a11048459c31499e57d6ce127a6cc7d95f211
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-06-07 19:12:56 +0200

    feat: Initializing of of objects and adding of new variables

--- Game/src/Start.java
+++ Game/src/Start.java
@@ -1,12 +1,20 @@
+import java.util.LinkedList;
+
 public class Start {
     public static Order[] orderList;
-    public int orderIndex = 0;
+    public static StorageHouse storageHouse;
+    public static int orderIndex = 0;
+    public static LinkedList<Order> activeOrderList;
+    public static int absoluteCash;
 
     public static void main(String[] args) {
-
         // Create a list of orders to manage the game main task
         orderList = Order.createOrderListFromFile(args[0]);
 
+        // Initialize activeOrderList
+        activeOrderList = new LinkedList<>();
 
+        // Initialize StorageHouse
+        storageHouse = new StorageHouse();
     }
 }
