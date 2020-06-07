bc50c39ff73aa5d1af64cb77eeb81e83ad8d6d3c
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-06-07 18:17:21 +0200

    feat: Adding of code to create a order list

--- Game/src/Start.java
+++ Game/src/Start.java
@@ -1,5 +1,12 @@
 public class Start {
+    public static Order[] orderList;
+    public int orderIndex = 0;
+
     public static void main(String[] args) {
+
+        // Create a list of orders to manage the game main task
+        orderList = Order.createOrderListFromFile(args[0]);
+
 
     }
 }
