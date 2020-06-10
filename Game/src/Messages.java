a070294c31c41faa0cb87e50cbb7d1f3b66576fe
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-06-10 21:08:38 +0200

    feat: Adding of many messages

---
+++ Game/src/Messages.java
@@ -0,0 +1,5 @@
+public class Messages {
+
+    public static String CANNOT_GET_MORE_ORDER_MESSAGE = "Es können keine weiteren Auträge gezogen werden.";
+    public static String STORAGE_FIELD_FULL_MESSAGE = "Dieses Feld ist bereits voll";
+}
