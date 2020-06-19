890f4515a939df3563f2369aef1140d4d6123fd5
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-06-19 10:37:43 +0200

    feat: Adding new class to manage game orders

---
+++ Game/src/OrderManager.java
@@ -0,0 +1,120 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class OrderManager {
+    private Order[] allOrders;
+    private ArrayList<Order> activeOrders;
+    private int selectedOrderIndex = 0, activeOrderCounter = 0, allOrderIndex = 0;
+    public static Order NULL_DUMMY = new Order("", "","","","");
+    public static int MAXIUMUM_STORAGE_SIZE = 9;
+
+    public OrderManager(Order[] allOrders) {
+        this.allOrders = allOrders;
+        this.activeOrders = new ArrayList<>();
+    }
+
+    public boolean addOrder(Order order) {
+        if (this.selectedOrderIndex < allOrders.length - 1) {
+            this.activeOrders.add(order);
+            this.selectedOrderIndex++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean removeOrder(Order order) {
+        if (!this.activeOrders.isEmpty()) {
+            this.activeOrders.remove(order);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void decreaseOrder() {
+        this.activeOrderCounter--;
+        this.selectedOrderIndex = 0;
+    }
+
+    public Order getLastOrder() {
+        return this.activeOrders.get(this.activeOrderCounter - 1);
+    }
+
+    public boolean isSelectedOrderFirst() {
+        if (this.selectedOrderIndex == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isSelectedOrderLast() {
+        if (this.selectedOrderIndex == 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Order getCurrentOrder() {
+        if (this.activeOrderCounter == 0) {
+            return NULL_DUMMY;
+        } else {
+            if (this.activeOrders.size() > 0 && this.activeOrders.size() + 1 >= this.selectedOrderIndex) {
+                return this.activeOrders.get(this.selectedOrderIndex);
+            } else {
+                return NULL_DUMMY;
+            }
+        }
+    }
+
+    public boolean hasOrders() {
+        if (this.activeOrders.size() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void loadNewOrder() {
+        if (this.activeOrders.size() < 3) {
+            this.activeOrders.add(this.allOrders[this.allOrderIndex]);
+            this.activeOrderCounter++;
+            this.selectedOrderIndex = this.activeOrderCounter - 1;
+            this.allOrderIndex++;
+        } else {
+            JOptionPane.showMessageDialog(null, Messages.CANNOT_GET_MORE_ORDER_MESSAGE);
+        }
+    }
+
+    public void selectNextOrder() {
+        if (this.selectedOrderIndex < 3) {
+            this.selectedOrderIndex++;
+        } else {
+            this.selectedOrderIndex = 2;
+        }
+    }
+
+    public void selectPrevOrder() {
+        if (this.selectedOrderIndex > 0) {
+            this.selectedOrderIndex--;
+        } else {
+            this.selectedOrderIndex = 0;
+        }
+    }
+
+    public boolean onlyStoreOrders() {
+        int counter = 0;
+        for (Order order : this.activeOrders) {
+            if (order.getOrderType() == Order.INCOMING_ORDER_STRING) {
+                counter++;
+            }
+        }
+        if (counter == this.activeOrders.size()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
