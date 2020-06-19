12cf77f7aae12510252234532ae6866cdd8256df
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-06-19 10:35:14 +0200

    feat: Adding new class to define all messages of the game

--- Game/src/Messages.java
+++ Game/src/Messages.java
@@ -2,4 +2,6 @@ public class Messages {
 
     public static String CANNOT_GET_MORE_ORDER_MESSAGE = "Es können keine weiteren Auträge gezogen werden.";
     public static String STORAGE_FIELD_FULL_MESSAGE = "Dieses Feld ist bereits voll";
+    public static String GAME_OVER_MESSAGE = "GAME OVER";
+    public static String UNDEFINED_ANSWER;
 }
