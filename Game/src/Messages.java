d8a6204c978fa8385a0e67629194bcd3639b3c38
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-06-20 14:27:52 +0200

    feat: Adding of new messages

--- Game/src/Messages.java
+++ Game/src/Messages.java
@@ -3,5 +3,9 @@ public class Messages {
     public static String CANNOT_GET_MORE_ORDER_MESSAGE = "Es können keine weiteren Auträge gezogen werden.";
     public static String STORAGE_FIELD_FULL_MESSAGE = "Dieses Feld ist bereits voll";
     public static String GAME_OVER_MESSAGE = "GAME OVER";
-    public static String UNDEFINED_ANSWER;
+    public static String UNDEFINED_ANSWER = "Status undefined";
+    public static String SELECT_STORAGE_TO_STORE = "Wählen sie einen Lagerplatz aus";
+    public static String SELECT_STORAGE_TO_DELIVER = "Wählen sie das zu versendende Objekt aus";
+    public static String SELECT_STORAGE_OBJECT_TO_MOVE = "Wählen sie das Objekt aus das umgelagert werden soll";
+    public static String SELECT_STORAGE_TARGET_OF_MOVE = "Wählen sie einen neuen Lagerplatz aus";
 }
