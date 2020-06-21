952cfa7e2c79726c2f1093561abde54fbebc662b
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-06-21 18:03:29 +0200

    feat: Adding of code to get standard orders

--- Game/src/AccountManager.java
+++ Game/src/AccountManager.java
@@ -5,8 +5,14 @@ public class AccountManager {
     private int account = 0;
     private OrderManager orderManager;
     private ArrayList<Order> transactionList;
+    private Order moveOrder;
+    private Order destroyOrder;
 
     public AccountManager(OrderManager orderManager) {
+        this.moveOrder = new Order("Umlagerung", "","",Order.MOVING_ORDER_STRING,
+                "-100");
+        this.destroyOrder = new Order("Zerstörung", "", "", Order.DESTROY_ORDER_STRING,
+                "-500");
         this.orderManager = orderManager;
         this.transactionList = new ArrayList<>();
     }
@@ -14,6 +20,22 @@ public class AccountManager {
     public void accountOrder(Order order) {
         this.account += order.getCash();
         this.transactionList.add(order);
+    }
+
+    public void accountMoveOrder() {
+        accountOrder(this.moveOrder);
+    }
+
+    public void accountDestroyOrder() {
+        accountOrder(this.destroyOrder);
+    }
+
+    public Order getDestroyOrder() {
+        return this.destroyOrder;
+    }
+
+    public Order getMoveOrder() {
+        return this.moveOrder;
     }
 
     public int getAccount() {
