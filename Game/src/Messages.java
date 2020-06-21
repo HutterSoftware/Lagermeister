637de67689434a28af88799442868afd7bcf32a3
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-06-21 19:42:59 +0200

    feat: Adding of new messages. Setting all messages to final

--- Game/src/Messages.java
+++ Game/src/Messages.java
@@ -1,11 +1,12 @@
 public class Messages {
 
-    public static String CANNOT_GET_MORE_ORDER_MESSAGE = "Es können keine weiteren Auträge gezogen werden.";
-    public static String STORAGE_FIELD_FULL_MESSAGE = "Dieses Feld ist bereits voll";
-    public static String GAME_OVER_MESSAGE = "GAME OVER";
-    public static String UNDEFINED_ANSWER = "Status undefined";
-    public static String SELECT_STORAGE_TO_STORE = "Wählen sie einen Lagerplatz aus";
-    public static String SELECT_STORAGE_TO_DELIVER = "Wählen sie das zu versendende Objekt aus";
-    public static String SELECT_STORAGE_OBJECT_TO_MOVE = "Wählen sie das Objekt aus das umgelagert werden soll";
-    public static String SELECT_STORAGE_TARGET_OF_MOVE = "Wählen sie einen neuen Lagerplatz aus";
+    public static final String CANNOT_DESTROY_ITS_EMPTY = "Lagerplatz ist schon leer";
+    public static final String CANNOT_GET_MORE_ORDER_MESSAGE = "Es können keine weiteren Auträge gezogen werden.";
+    public static final String STORAGE_FIELD_FULL_MESSAGE = "Dieses Feld ist bereits voll";
+    public static final String GAME_OVER_MESSAGE = "GAME OVER";
+    public static final String UNDEFINED_ANSWER = "Status undefined";
+    public static final String SELECT_STORAGE_TO_STORE = "Wählen sie einen Lagerplatz aus";
+    public static final String SELECT_STORAGE_TO_DELIVER = "Wählen sie das zu versendende Objekt aus";
+    public static final String SELECT_STORAGE_OBJECT_TO_MOVE = "Wählen sie das Objekt aus das umgelagert werden soll";
+    public static final String SELECT_STORAGE_TARGET_OF_MOVE = "Wählen sie einen neuen Lagerplatz aus";
 }
