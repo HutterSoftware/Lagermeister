63197ffe97e383c2b551d23f9345f828fa95ee41
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-06-29 19:47:34 +0200

    feat: Adding of new methods

--- Game/src/AccountManager.java
+++ Game/src/AccountManager.java
@@ -1,17 +1,16 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class AccountManager {
-    private int account = 0;
+    private int account = 0, turnover = 0, costs = 0, win = 0;
     private OrderManager orderManager;
     private ArrayList<Order> transactionList;
-    private Order moveOrder;
-    private Order destroyOrder;
+    public static Order MOVE_ORDER;
+    public static Order DESTROY_ORDER;
 
     public AccountManager(OrderManager orderManager) {
-        this.moveOrder = new Order("Umlagerung", "","",Order.MOVING_ORDER_STRING,
+        MOVE_ORDER = new Order("Umlagerung", "","",Order.MOVING_ORDER_STRING,
                 "-100");
-        this.destroyOrder = new Order("Zerstörung", "", "", Order.DESTROY_ORDER_STRING,
+        DESTROY_ORDER = new Order("Zerstörung", "", "", Order.DESTROY_ORDER_STRING,
                 "-500");
         this.orderManager = orderManager;
         this.transactionList = new ArrayList<>();
@@ -19,26 +18,33 @@ public class AccountManager {
 
     public void accountOrder(Order order) {
         this.account += order.getCash();
+        this.turnover += Math.abs(order.getCash());
+        if (order.getCash() < 0) {
+            this.costs += Math.abs(order.getCash());
+            this.win = this.turnover - this.costs;
+            this.turnover += Math.abs(order.getCash());
+        }
+
         this.transactionList.add(order);
-    }
-
-    public void accountMoveOrder() {
-        accountOrder(this.moveOrder);
-    }
-
-    public void accountDestroyOrder() {
-        accountOrder(this.destroyOrder);
-    }
-
-    public Order getDestroyOrder() {
-        return this.destroyOrder;
-    }
-
-    public Order getMoveOrder() {
-        return this.moveOrder;
     }
 
     public int getAccount() {
         return this.account;
+    }
+
+    public ArrayList<Order> getAllTransactions() {
+        return this.transactionList;
+    }
+
+    public int getTurnover() {
+        return turnover;
+    }
+
+    public int getCosts() {
+        return costs;
+    }
+
+    public int getWin() {
+        return win;
     }
 }
