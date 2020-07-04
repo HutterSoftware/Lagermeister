970e8c0c92658e0a4ff63417adf26587a734d22c
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-07-04 20:35:56 +0200

    feat: Adding javadoc

--- Game/src/Storage.java
+++ Game/src/Storage.java
@@ -2,18 +2,30 @@ import javax.swing.*;
 import java.util.Stack;
 
 public class Storage {
+
+    // Class attributes
     private Stack storage;
 
+    /**
+     * Initialize object
+     */
     public Storage() {
         this.storage = new Stack();
     }
 
+    /**
+     * Resetting storage
+     */
     public void reset() {
         while (!storage.empty()) {
             storage.pop();
         }
     }
 
+    /**
+     * Getting the top of stack
+     * @return Order
+     */
     public Order viewTopOrder() {
         if (this.storage.empty()) {
             return null;
@@ -22,24 +34,38 @@ public class Storage {
         }
     }
 
+    /**
+     * Adding new Order to stack
+     * @param order Order
+     * @return boolean
+     */
     public boolean addOrder(Order order) {
-        if (this.storage.size() < 3) {
-            this.storage.push(order);
-            return true;
-        } else{
+        if (this.storage.size() == 3) {
             return false;
-        }
-    }
-
-    public boolean removeTopElement() {
-        if (!this.storage.empty()) {
-            this.storage.pop();
-            return true;
         } else {
-            return false;
+            this.storage.addElement(order);
+            return true;
         }
     }
 
+    /**
+     * Removing top element of stack
+     * @return boolean
+     */
+    public boolean removeTopElement() {
+        if (this.storage.empty()) {
+            return false;
+        }
+
+        this.storage.pop();
+        return true;
+    }
+
+    /**
+     * Equalizing two Order objects
+     * @param order Order
+     * @return boolean
+     */
     private boolean equal(Order order) {
         for (int i = 0; i < this.storage.size(); i++) {
             Order checkObject = (Order)this.storage.elementAt(i);
@@ -53,15 +79,20 @@ public class Storage {
         return false;
     }
 
+    /**
+     * Searching objects in stack
+     * @param order Order
+     * @return boolean
+     */
     public boolean containsProduct(Order order) {
-
-        if (equal(order)) {
-            return true;
-        } else {
-            return false;
-        }
+        return equal(order);
     }
 
+    /**
+     * Checking top element with given Order object
+     * @param order Order
+     * @return boolean
+     */
     public boolean isOrderAtTop(Order order) {
         if (this.storage.isEmpty()) {
             return false;
@@ -69,16 +100,15 @@ public class Storage {
 
         Order peekOrder = (Order)this.storage.peek();
 
-        if  (peekOrder.getProductName().equals(order.getProductName())
+        return  (peekOrder.getProductName().equals(order.getProductName())
                 && peekOrder.getAttribute1().equals(order.getAttribute1())
-                && peekOrder.getAttribute2().equals(order.getAttribute2())) {
-
-            return true;
-        } else {
-            return false;
-        }
+                && peekOrder.getAttribute2().equals(order.getAttribute2()));
     }
 
+    /**
+     * Getting size of stack
+     * @return int
+     */
     public int getStorageSize() {
         if (this.storage.empty()) {
             return 0;
@@ -95,6 +125,9 @@ public class Storage {
         }
     }
 
+    /**
+     * Destroying of top element in stack
+     */
     public void destroyTop() {
         if (!this.storage.empty()){
             this.storage.pop();
@@ -103,6 +136,10 @@ public class Storage {
         }
     }
 
+    /**
+     * Creating String from object
+     * @return String
+     */
     @Override
     public String toString() {
         String t = "";
